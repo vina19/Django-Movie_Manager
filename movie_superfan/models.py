f1b0419a5d4999693f0cf933fb5eb6f9628ef529
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-12-08 16:07:36 -0600

    Create movie models

---
+++ movie_superfan/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create Movie model
+class Movie(models.Model):
+    name = models.CharField(max_length=300)
+    year = models.CharField(max_length=5)
+    plot = models.CharField(max_length=1500)
+    imdb_rating = models.FloatField(default=0.0)
+    poster = models.ImageField(upload_to='movie_posters/', blank=True, null=True)
+    watched = models.BooleanField(default=False)
+    trailer = models.CharField(max_length=150, default=None, null=True, blank=True)
+
+    def __str__(self):
+        poster_str = self.poster.url if self.poster else 'no poster'
+        return self.name
