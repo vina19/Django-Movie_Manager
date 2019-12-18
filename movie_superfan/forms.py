ad71b1a79742adf68b305dae4bde32ea3c4af11a
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-12-18 17:18:55 -0600

    Fixed the registration form to use CustomUser model

--- movie_superfan/forms.py
+++ movie_superfan/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ValidationError
-from .models import Comment
+from .models import Comment, CustomUser
 
 class UserRegisterForm(UserCreationForm):
 
     email = forms.EmailField()
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'email', 'password1', 'password2']
     
     def clean_email(self):
@@ -17,7 +17,7 @@ class UserRegisterForm(UserCreationForm):
         if not email:
             raise ValidationError('Please enter an email address')
 
-        if User.objects.filter(email__iexact=email).exists():
+        if CustomUser.objects.filter(email__iexact=email).exists():
             raise ValidationError('A user has already registered using this email')
 
         return email
